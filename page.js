eb50215f4669ae6dc65d050e8675140cd002a019
Kamil <kamilshape@gmail.com>
2020-02-24 07:06:54 +0100

    Initial commit

---
+++ page.js
@@ -0,0 +1,5 @@
+const arrow2 = document.querySelector('.header__arrow:nth-of-type(2)')
+const arrow3 = document.querySelector('.header__arrow:nth-of-type(3)')
+
+console.log(arrow2)
+console.log(arrow3)
