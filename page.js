41dcc87e3c4e7e6a8a6d7e63ef5de971bb73aa11
Kamil <kamilshape@gmail.com>
2020-03-04 18:19:26 +0100

    Media Queries 2

--- page.js
+++ page.js
@@ -1,9 +1,15 @@
 const hambButton = document.querySelector('.hamburger')
 const menu = document.querySelector('.menu')
+const topBar = document.querySelector('.hamburger__bar-top')
+const middleBar = document.querySelector('.hamburger__bar-middle')
+const bottomBar = document.querySelector('.hamburger__bar-bottom')
 
 const hambFunction = () => {
     menu.classList.toggle('menu--active')
     hambButton.classList.toggle('hamburger--active')
+    topBar.classList.toggle('hamburger__bar-top--active')
+    middleBar.classList.toggle('hamburger__bar-middle--active')
+    bottomBar.classList.toggle('hamburger__bar-bottom--active')
 }
 
 hambButton.addEventListener('click', hambFunction)
