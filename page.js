26d64d95adac568572448a4ed2ac66e434e84546
Kamil <kamilshape@gmail.com>
2020-03-24 11:02:58 +0100

    Rename

--- page.js
+++ page.js
@@ -1,3 +1,4 @@
+//Hamburger Menu
 const hambButton = document.querySelector('.hamburger')
 const menu = document.querySelector('.menu')
 const topBar = document.querySelector('.hamburger__bar-top')
@@ -13,3 +14,77 @@ const hambFunction = () => {
 }
 
 hambButton.addEventListener('click', hambFunction)
+
+//Drop-down menu
+const blogLink = document.querySelector('.menu__list-item-link--blog')
+const servicesLink = document.querySelector('.menu__list-item-link--services')
+
+blogLink.addEventListener('mouseover', function () {
+    const blogList = document.querySelector('.menu__inside-list--blog')
+    blogList.classList.toggle('menu__list-item-link--active')
+})
+servicesLink.addEventListener('mouseover', function () {
+    const servicesList = document.querySelector('.menu__inside-list--services')
+    servicesList.classList.toggle('menu__list-item-link--active')
+})
+
+//Scrolling - appear
+const New = document.querySelector('.news__new')
+const header = document.querySelector('.header')
+const about = document.querySelector('.about')
+const news = document.querySelector('.news')
+const services = document.querySelector('.services')
+const contact = document.querySelector('.contact')
+const kitchen = document.querySelector('.kitchen')
+const windowSize = window.innerHeight
+const firstHeight = header.clientHeight + about.clientHeight
+const secondHeight = header.clientHeight + about.clientHeight + news.clientHeight
+const articlesNew = [...document.getElementsByClassName('news__new')];
+const articlesKitchen = [...document.getElementsByClassName('kitchen__new ')]
+
+
+
+const appearNew = () => {
+    let scrolling = window.scrollY
+    console.log(scrolling)
+    for (i = 0; i < articlesNew.length; i++) {
+        if (scrolling > firstHeight + articlesNew[i].offsetTop) {
+            articlesNew[i].classList.add('article--active')
+            // console.log(articles[i].offsetTop)
+        }
+
+    }
+}
+window.addEventListener('scroll', appearNew)
+
+const appearKitchen = () => {
+    let scrolling = window.scrollY
+    console.log(scrolling)
+    for (i = 0; i < articlesKitchen.length; i++) {
+        if (scrolling > secondHeight + articlesKitchen[i].offsetTop) {
+            articlesKitchen[i].classList.add('article--active')
+        }
+
+    }
+}
+window.addEventListener('scroll', appearKitchen)
+
+
+const headerPosition = header.offsetTop
+const aboutPosition = about.offsetTop
+const contactPosition = contact.offsetTop
+
+//Smooth scroll
+
+// $('.menu__list-item-link--home').on('click', function (e) {
+//     $('body,html').animate({
+//         scrollTop: $('.header').offset().top
+//     }, 1000)
+// })
+
+$('.menu__list-item-link').on('click', function () {
+    const section = '.' + $(this).attr('id')
+    $('body,html').animate({
+        scrollTop: $(section).offset().top
+    })
+})
